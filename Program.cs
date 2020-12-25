bf7632929a69a157f57a8e117a39dc55f3220de1
H-Soba Vincent <hsobav@gmail.com>
2020-12-24 22:28:37 -0500

    Unhandled Exception: System.IndexOutOfRangeException: Index was outside the bounds of the array.    at SelectionSort.SelectionSort.Sort(Int32[] inputArray) in C:\Users\hvincent\source\repos\SelectionSort\SelectionSort.cs:line 27    at SelectionSort.Program.Main(String[] args) in C:\Users\hvincent\source\repos\SelectionSort\Program.cs:line 22

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace SelectionSort
     {
         static void Main(string[] args)
         {
+            int[] exampleArray = new int[] { 93, 23, 12, 64, 22, 68, 47 };
+
+            Console.Write("Before sorting: ");
+
+            foreach (int element in exampleArray)
+            {
+                Console.Write(element + " ");
+            }
+
+            SelectionSort.Sort(exampleArray);
+
+            Console.WriteLine();
+            Console.Write("After sorting: ");
+
+            foreach (int element in exampleArray)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+
         }
     }
 }
--- SelectionSort.cs
+++ SelectionSort.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace SelectionSort
 {
-    class SelectionSort
+    public class SelectionSort
     {
+        private static bool More(int value1, int value2)
+        {
+            return value1 > value2;
+        }
+
+        public static void Sort(int[] inputArray)
+        {
+            int arraySize = inputArray.Length;
+
+            int i, j, temp;
+            int max = 0;
+
+            for (i = 0; i < arraySize - 1; i++)     // loop through the whole array except last index
+            {
+                for (j = 0; j < arraySize - i - 1; j++)
+                {
+                    if (More(inputArray[j], inputArray[j + 1]) && max < inputArray[arraySize - i])
+                    {
+                        max = inputArray[j];
+                    }
+                }
+
+                temp = inputArray[arraySize - i];
+                inputArray[arraySize - i] = inputArray[j];
+                inputArray[j] = temp;
+
+
+            }
+        }
     }
 }
